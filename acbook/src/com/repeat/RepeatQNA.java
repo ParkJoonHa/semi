f4fba21a438ec6257a6c06e8d71dec3ae9a633bf
SIST <SIST@DESKTOP-5CK136Q>
2020-11-04 14:27:15 +0900

    자주하는질문 게시판 추가 RepeatQNA - 도현승

---
+++ acbook/src/com/repeat/RepeatQNA.java
@@ -0,0 +1,59 @@
+package com.repeat;
+/**
+ * repreatNum : 자주하는 질문 게시판 글번호(기본키)
+ * userId : 아이디(외래키)
+ * userName : 이름
+ * subject : 제목
+ * content : 내용
+ * created : 글 등록일
+ * @author SIST
+ *
+ */
+public class RepeatQNA {
+	private int repeatNum;
+	private String userId;
+	private String userName;
+	private String subject;
+	private String content;
+	private String created;
+	
+	
+	public int getRepeatNum() {
+		return repeatNum;
+	}
+	public void setRepeatNum(int repeatNum) {
+		this.repeatNum = repeatNum;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCreated() {
+		return created;
+	}
+	public void setCreated(String created) {
+		this.created = created;
+	}
+	
+	
+}
