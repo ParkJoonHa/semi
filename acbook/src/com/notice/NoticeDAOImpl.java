b7a2700b55390de4c6ce2ef3e4d9f281c98a64ae
AN-JEONGHYEON <AN-JEONGHYEON@DESKTOP-HVUOUT6>
2020-11-05 21:57:21 +0900

    공지사항 수정 - 정현

--- acbook/src/com/notice/NoticeDAOImpl.java
+++ acbook/src/com/notice/NoticeDAOImpl.java
@@ -4,88 +4,93 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.util.DBConn;
 
-public class NoticeDAOImpl implements NoticeDAO{
+public class NoticeDAOImpl implements NoticeDAO {
 	private Connection conn = DBConn.getConnection();
 
 	@Override
 	public int insertNotice(NoticeDTO dto) throws SQLException {
-		int result=0;
-		StringBuilder sb=new StringBuilder();
-		PreparedStatement pstmt=null;
+		int result = 0;
+		StringBuilder sb = new StringBuilder();
+		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			// 먼저 시퀀스 값 가져와서 num에 대입 (시퀀스 currval로 가져올 시 문제생길 수 있어서 먼저하기)
-			
+			sb.append("SELECT notice_number.NEXTVAL FROM dual ");
+			pstmt = conn.prepareStatement(sb.toString());
+			rs = pstmt.executeQuery();
+			int noticeNum = rs.getInt(1);
+
+			pstmt.close();
+
 			// 내용 테이블 추가
-			
-			if (dto.getSaveFiles()!=null) {
+			if (dto.getSaveFiles() != null) { // 첨부 파일이 있으면
 				for (int i = 0; i < dto.getSaveFiles().length; i++) {
 					dto.setSaveFilename(dto.getSaveFiles()[i]);
 					dto.setOriginalFilename(dto.getOriginalFiles()[i]);
 					// 아래에 작성한 파일테이블에 추가하는 메소드 호출
-					// insertFile(dto); 
+					insertFile(dto, noticeNum);
 				}
 			}
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			sb.append("INSERT INTO notice ");
-			sb.append(" (noticeNum, userId, subject, content, hitCount, created) ");
-			sb.append(" VALUES (notice_seq.NEXTVAL, ?, ?, ?, 0, SYSDATE) ");
-			
-			pstmt=conn.prepareStatement(sb.toString());
+			sb.append(" (userId, subject, content, hitCount, created) ");
+			sb.append(" VALUES (?, ?, ?, 0, SYSDATE) ");
+
+			pstmt = conn.prepareStatement(sb.toString());
 			pstmt.setString(1, dto.getUserId());
 			pstmt.setString(2, dto.getSubject());
 			pstmt.setString(3, dto.getContent());
-			
-			result = pstmt.executeUpdate();
 
-			pstmt.close();
+			result = pstmt.executeUpdate();
 			
-			sb.append("SELECT notice_number.CURRVAL FROM dual ");
-				// 이렇게 currval 써두 되는지..?
-			pstmt = conn.prepareStatement(sb.toString());
-			rs = pstmt.executeQuery();
-			int noticeNum = rs.getInt(1);
-			
-			
-			if (dto.getOriginalFilename()!=null) { 
-				// 파일이 있는지?? 확인하기 위한 조건문으론 무엇을 넣어야 할까?
-				
-				sb.append("INSERT INTO multifile ");
-				sb.append(" (fileNum, noticeNum, saveFilename, originalFilename) ");
-				sb.append(" VALUES (notice_file_seq.NEXTVAL, ?, ?, ? ) ");
-				
-				pstmt = conn.prepareStatement(sb.toString());
-				pstmt.setInt(1, noticeNum);
-				pstmt.setString(2, dto.getSaveFilename());
-				pstmt.setString(3, dto.getOriginalFilename());
-				
-				result =pstmt.executeUpdate();					
-			}			
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			if(rs!=null) {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (SQLException e) {
 				}
 			}
-			if(pstmt!=null) {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		return result;
+	}
+	
+	@Override
+	public int insertFile(NoticeDTO dto, int noticeNum) throws SQLException {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "INSERT INTO multiFile "
+					+ "  (fileNum, noticeNum, saveFilename, originalFilename) "
+					+ "   VALUES (notice_file_seq.NEXTVAL, ?, ?, ? ) ";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, noticeNum);
+			pstmt.setString(2, dto.getSaveFilename());
+			pstmt.setString(3, dto.getOriginalFilename());
+
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (SQLException e) {
@@ -109,26 +114,206 @@ public class NoticeDAOImpl implements NoticeDAO{
 
 	@Override
 	public int dataCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		int result =0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		try {
+			sql = "SELECT NVL(COUNT(*),0) FROM notice";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				result=rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+				
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		
+		return result;
 	}
 
 	@Override
 	public int dataCount(String condition, String keyword) {
-		// TODO Auto-generated method stub
-		return 0;
+        int result=0;
+        PreparedStatement pstmt=null;
+        ResultSet rs=null;
+        String sql;
+
+        try {
+        	if(condition.equalsIgnoreCase("created")) {
+        		keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+        		sql="SELECT NVL(COUNT(*), 0) FROM notice n JOIN member1 m ON n.userId=m.userId WHERE TO_CHAR(created, 'YYYYMMDD') = ?  ";
+        	} else {
+        		sql="SELECT NVL(COUNT(*), 0) FROM notice n JOIN member1 m ON n.userId=m.userId WHERE  INSTR(" + condition + ", ?) >= 1 ";
+        	}
+        	
+            pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1, keyword);
+
+            rs=pstmt.executeQuery();
+
+            if(rs.next()) {
+                result=rs.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+				
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+        
+        return result;
 	}
 
 	@Override
 	public List<NoticeDTO> listNotice(int offset, int rows) {
-		// TODO Auto-generated method stub
-		return null;
+		List<NoticeDTO> list=new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		StringBuilder sb=new StringBuilder();
+		
+		try {
+			sb.append("SELECT n.noticeNum, n.userId, userName, subject, saveFilename, ");
+			sb.append("       hitCount, created  ");
+			sb.append(" FROM notice n "
+					+ " JOIN member1 m ON n.userId=m.userId ");
+			sb.append(" LEFT OUTER JOIN multiFile f ON n.noticeNum=f.noticeNum");
+			sb.append(" ORDER BY num DESC  ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, offset);
+			pstmt.setInt(2, rows);
+			
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				NoticeDTO dto=new NoticeDTO();
+				
+				dto.setNoticeNum(rs.getInt("num"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created"));
+				
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+				
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		
+		return list;
 	}
 
 	@Override
 	public List<NoticeDTO> listNotice(int offset, int rows, String condition, String keyword) {
-		// TODO Auto-generated method stub
-		return null;
+        List<NoticeDTO> list=new ArrayList<NoticeDTO>();
+
+        PreparedStatement pstmt=null;
+        ResultSet rs=null;
+        StringBuilder sb=new StringBuilder();
+
+        try {
+			sb.append("SELECT n.noticeNum, n.userId, userName, subject, saveFilename, ");
+			sb.append("       hitCount, created  ");
+			sb.append(" FROM notice n "
+					+ " JOIN member1 m ON n.userId=m.userId  ");
+			sb.append(" LEFT OUTER JOIN multiFile f ON n.noticeNum = f.noticeNum  ");
+			
+			if(condition.equalsIgnoreCase("created")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sb.append(" WHERE TO_CHAR(created, 'YYYYMMDD') = ?  ");
+			} else {
+				sb.append(" WHERE INSTR(" + condition + ", ?) >= 1  ");
+			}
+			sb.append(" ORDER BY num DESC  ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY");
+            
+			pstmt=conn.prepareStatement(sb.toString());
+            
+			pstmt.setString(1, keyword);
+			pstmt.setInt(2, offset);
+			pstmt.setInt(3, rows);
+            
+            rs=pstmt.executeQuery();
+            
+            while(rs.next()) {
+				NoticeDTO dto=new NoticeDTO();
+				
+				dto.setNoticeNum(rs.getInt("noticeNum"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created")); // yyyy-MM-dd HH:mm:ss
+                
+                list.add(dto);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+				
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+        
+        return list;
 	}
 
 	@Override
@@ -139,8 +324,56 @@ public class NoticeDAOImpl implements NoticeDAO{
 
 	@Override
 	public NoticeDTO readNotice(int num) {
-		// TODO Auto-generated method stub
-		return null;
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		
+		sql = "SELECT noticeNum, n.userId, userName, subject, content, saveFilename,originalFilename, filesize, hitCount, created ";
+		sql+= "  FROM notice n "
+				+ " JOIN member1 m ON n.userId=m.userId "
+				+ " LEFT OUTER JOIN mulfiFile f ON n.noticeNum=f.noticeNum "
+				+ " WHERE num = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			
+			rs = pstmt.executeQuery();
+			
+			if( rs.next()) {
+				dto = new NoticeDTO();
+				
+				dto.setNoticeNum(rs.getInt("noticeNum"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setOriginalFilename(rs.getString("originalFilename"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setCreated(rs.getString("created"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+				
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		
+		return dto;
 	}
 
 	@Override
--- acbook/src/com/notice/NoticeServlet.java
+++ acbook/src/com/notice/NoticeServlet.java
@@ -2,7 +2,11 @@ package com.notice;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -11,143 +15,297 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 
 import com.member.SessionInfo;
 import com.util.MyUploadServlet;
+import com.util.MyUtil;
 
 @MultipartConfig
 @WebServlet("/notice/*")
-public class NoticeServlet extends MyUploadServlet{
+public class NoticeServlet extends MyUploadServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String pathname;
 
 	@Override
 	protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
-		
+
 		String cp = req.getContextPath();
 		String uri = req.getRequestURI();
-		
+
 		HttpSession session = req.getSession();
-		SessionInfo info = (SessionInfo)session.getAttribute("member");
-		
-		if(uri.indexOf("list.do") == -1 && info == null ) {
-			resp.sendRedirect(cp+"/member/login.do");
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		if (uri.indexOf("list.do") == -1 && info == null) {
+			resp.sendRedirect(cp + "/member/login.do");
 			return;
 		}
-		
+
 		// 파일 저장 경로(pathname)
 		String root = session.getServletContext().getRealPath("/");
-		pathname = root+"uploads"+File.separator+"notice";
-		
-		if (uri.indexOf("list.do")!=-1) {
+		pathname = root + "uploads" + File.separator + "notice";
+
+		if (uri.indexOf("list.do") != -1) {
 			list(req, resp);
-		} else if (uri.indexOf("created.do")!=-1) {
+		} else if (uri.indexOf("created.do") != -1) {
 			createdForm(req, resp);
-		} else if (uri.indexOf("created_ok.do")!=-1) {
+		} else if (uri.indexOf("created_ok.do") != -1) {
 			createdSubmit(req, resp);
-		} else if (uri.indexOf("article.do")!=-1) {
+		} else if (uri.indexOf("article.do") != -1) {
 			article(req, resp);
-		} else if (uri.indexOf("update.do")!=-1) {
+		} else if (uri.indexOf("update.do") != -1) {
 			updateForm(req, resp);
-		} else if (uri.indexOf("update_ok.do")!=-1) {
+		} else if (uri.indexOf("update_ok.do") != -1) {
 			updateSubmit(req, resp);
-		} else if (uri.indexOf("deleteFile.do")!=-1) {
+		} else if (uri.indexOf("deleteFile.do") != -1) {
 			deleteFile(req, resp);
-		} else if (uri.indexOf("delete.do")!=-1) {
+		} else if (uri.indexOf("delete.do") != -1) {
 			delete(req, resp);
-		} else if (uri.indexOf("download.do")!=-1) {
+		} else if (uri.indexOf("download.do") != -1) {
 			download(req, resp);
-		} else if (uri.indexOf("deleteList.do")!=-1) {			
+		} else if (uri.indexOf("deleteList.do") != -1) {
 			deleteList(req, resp);
-		}		
+		}
 	}
-	
+
 	private void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		NoticeDAO dao = new NoticeDAOImpl();
+		MyUtil util = new MyUtil();
+		String cp = req.getContextPath();
+
+		String page = req.getParameter("page");
+		int current_page = 1;
+		if (page != null) {
+			current_page = Integer.parseInt(page);
+		}
+
+		String condition = req.getParameter("condition");
+		String keyword = req.getParameter("keyword");
+		if (condition == null) {
+			condition = "subject";
+			keyword = "";
+		}
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			keyword = URLDecoder.decode(keyword, "utf-8");
+		}
+
+		// 한페이지 표시할 데이터 개수
+        String numPerPage=req.getParameter("rows");
+        int rows = numPerPage == null ? 10 : Integer.parseInt(numPerPage);
+        
+		int dataCount, total_page;
 		
+		if(keyword.length()!=0) {
+			dataCount= dao.dataCount(condition, keyword);
+		} else {
+			dataCount= dao.dataCount();
+		}
+		total_page=util.pageCount(rows, dataCount);
+		
+		if(current_page>total_page)
+			current_page=total_page;
+		
+		int offset=(current_page-1)*rows;
+		
+		List<NoticeDTO> list;
+		if(keyword.length()!=0) {
+			list= dao.listNotice(offset, rows, condition, keyword);
+		} else {
+			list= dao.listNotice(offset, rows);
+		}
+		
+		// 공지글
+		// List<NoticeDTO> listNotice=null;
+		// listNotice = dao.listNotice();
+		// for(NoticeDTO dto : listNotice){
+		// 	dto.setCreated(dto.getCreated().substring(0, 10));
+		//}
+
+		   long gap;
+		   Date curDate = new Date();
+		   SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		// 리스트 글번호 만들기
+		int listNum, n=0;
+		for(NoticeDTO dto : list){
+			listNum=dataCount-(offset+n);
+			dto.setListNum(listNum);
+			
+			try {
+				Date date= sdf.parse(dto.getCreated());
+				
+				gap = (curDate.getTime() - date.getTime()) /(1000*60*60*24); // 일자
+				gap = (curDate.getTime() - date.getTime()) /(1000*60*60); // 시간 
+				dto.setGap(gap);
+			}catch (Exception e) {
+			}
+			
+			dto.setCreated(dto.getCreated().substring(0, 10));
+			n++;
+		}
+		
+		String query="";
+		String listUrl;
+		String articleUrl;
+		
+		listUrl=cp+"/notice/list.do?rows="+rows;
+		articleUrl=cp+"/notice/article.do?page=" +current_page+"&rows="+rows;
+		if(keyword.length()!=0) {
+			query="condition="+condition+"&keyword="+URLEncoder.encode(keyword,"utf-8");
+			
+			listUrl += "&"+query;
+			articleUrl += "&"+query;
+		}
+		
+		String paging=util.paging(current_page, total_page, listUrl);
+		
+		// 포워딩 jsp에 넘길 데이터
+		req.setAttribute("list", list);
+		//req.setAttribute("listNotice", listNotice);
+		req.setAttribute("articleUrl", articleUrl);
+		req.setAttribute("dataCount", dataCount);
+		req.setAttribute("page", current_page);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("paging", paging);
+		req.setAttribute("condition", condition);
+		req.setAttribute("keyword", keyword);
+		req.setAttribute("rows", rows);
+		
+		// JSP로 포워딩
+		forward(req, resp, "/WEB-INF/views/notice/list.jsp");
 	}
+
 	private void createdForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		SessionInfo info=(SessionInfo)session.getAttribute("member");
-		
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		String cp = req.getContextPath();
+		String rows = req.getParameter("rows");
+
+		if (!info.getUserId().equals("admin")) {
+			resp.sendRedirect(cp + "/notice/list.do?rows=" + rows);
+			return;
+		}
+
+		req.setAttribute("mode", "created");
+		req.setAttribute("rows", rows);
+		forward(req, resp, "WEB-INF/views/notice/created.jsp");
+	}
+
+	private void createdSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
 		NoticeDAO dao = new NoticeDAOImpl();
 		String cp = req.getContextPath();
 		String rows = req.getParameter("rows");
-		
-		if (! info.getUserId().equals("admin")) {
-			resp.sendRedirect(cp+"/notice/list.do?rows="+rows);
+
+		if (!info.getUserId().equals("admin")) {
+			resp.sendRedirect(cp + "/notice/list.do?rows=" + rows);
 			return;
 		}
-		
+
 		try {
 			NoticeDTO dto = new NoticeDTO();
 			dto.setUserId(info.getUserId());
-			//if(req.getParameter("notice")!=null) { // 공지 선택시 상단에 띄울지
-				//dto.setNotice(Integer.parseInt(req.getParameter("notice")));
-			//}
+			// if(req.getParameter("notice")!=null) { // 공지 선택시 상단에 띄울지
+			// dto.setNotice(Integer.parseInt(req.getParameter("notice")));
+			// }
 			dto.setSubject(req.getParameter("subject"));
 			dto.setContent(req.getParameter("content"));
-			
-			
-			/*
-			for(Part p : req.getParts()) { 
-				String contentType = p.getContentType();
 
-				if(contentType!=null) { // 파일이면
-					Map<String, String> map = doFileUpload(p, pathname);
-					if(map==null) continue;
-					String saveFilename = map.get("saveFilename");
-					String originalFilename = map.get("saveFilename");
-				} else { 
-					// 파일이 아니면(<input t ype="text"... 등)
-					continue;
-				}
-			}
-			*/
-			
-			//Part p = req.getPart("upload"); // 여러개 넘겨줄 파일 객체 이름 ? 아닌가 강사님 지우심
-			
-			Map<String, String[]>map = doFileUpload(req.getParts(), pathname);
-			if (map!=null) {
+			Map<String, String[]> map = doFileUpload(req.getParts(), pathname);
+			if (map != null) {
 				dto.setSaveFiles(map.get("saveFilenames"));
 				dto.setOriginalFiles(map.get("originalFilenames"));
 			}
-			
-			
-			
+
 			dao.insertNotice(dto);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
+		resp.sendRedirect(cp + "/notice/list.do?rows=" + rows);
 	}
-	private void createdSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-	}
+
 	private void article(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		NoticeDAO dao = new NoticeDAOImpl();
+		String cp = req.getContextPath();
+
+		String page = req.getParameter("page");
+		String rows = req.getParameter("rows");
+		String query = "page=" + page + "&rows=" + rows;
+
+		try {
+			int num = Integer.parseInt(req.getParameter("num"));
+
+			String condition = req.getParameter("condition");
+			String keyword = req.getParameter("keyword");
+			if (condition == null) {
+				condition = "subject";
+				keyword = "";
+			}
+			keyword = URLDecoder.decode(keyword, "utf-8");
+
+			if (keyword.length() != 0) {
+				query += "&condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "UTF-8");
+			}
+
+			// 조회수
+			dao.updateHitCount(num);
+
+			// 게시물 가져오기
+			NoticeDTO dto = dao.readNotice(num);
+			if (dto == null) {
+				resp.sendRedirect(cp + "/notice/list.do?" + query);
+				return;
+			}
+
+			dto.setContent(dto.getContent().replaceAll("\n", "<br>"));
+
+			// 이전글/다음글
+			// NoticeDTO preReadDto = dao.preReadNotice(dto.getNum(), condition, keyword);
+			// NoticeDTO nextReadDto = dao.nextReadNotice(dto.getNum(), condition, keyword);
+
+			req.setAttribute("dto", dto);
+			// req.setAttribute("preReadDto", preReadDto);
+			// req.setAttribute("nextReadDto", nextReadDto);
+			req.setAttribute("query", query);
+			req.setAttribute("page", page);
+			req.setAttribute("rows", rows);
+
+			forward(req, resp, "/WEB-INF/views/notice/article.jsp");
+			return;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		resp.sendRedirect(cp + "/notice/list.do?" + query);
 	}
+
 	private void updateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
+
 	private void updateSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
+
 	private void deleteFile(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
+
 	private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
+
 	private void download(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
+
 	private void deleteList(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
-	
-	
+
 }
