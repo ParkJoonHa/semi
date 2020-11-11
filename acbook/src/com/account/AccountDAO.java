fec1593571855244c412aeea0863ca4b7539eaf2
INAEKIM <INAEKIM@LAPTOP-15IE9862>
2020-11-11 18:18:37 +0900

    일일, 월별 리스트 화면보기 가능.세부적인건 수정 필요

--- acbook/src/com/account/AccountDAO.java
+++ acbook/src/com/account/AccountDAO.java
@@ -7,13 +7,17 @@ public interface AccountDAO {
 
 	public  int insertAccount(AccountDTO dto) throws SQLException;
 	public  int updateAccount(AccountDTO dto) throws SQLException;
-	public  int deleteAccount(int num, String userId) throws SQLException;
+	public  int deleteAccount(int abNum, String userId) throws SQLException;
 
-	public List<AccountDTO > listAccount(int offset, int rows);
-	public List<AccountDTO > listAccount(int offset, int rows, String condition, String keyword);
+	public int dataCount(String userId);
+	public int dataCount(String userId, String condition, String keyword);
+	
+	public List<AccountDTO > listAccount(int offset, int rows, String userId);
+	public List<AccountDTO > listAccount(String userId, String date);
+	public List<AccountDTO > listAccount(int offset, int rows, String condition, String keyword, String userId);
 
-	public AccountDTO readAccount(int num);
-	public AccountDTO preReadAccount(int num, String condition, String keyword);
-	public AccountDTO nextReadAccount(int num, String condition, String keyword);
+	public AccountDTO readAccount(int abNum);
+	public AccountDTO preReadAccount(int abNum, String condition, String keyword);
+	public AccountDTO nextReadAccount(int abNum, String condition, String keyword);
 	
 }
